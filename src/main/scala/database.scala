14acf69f5b265ab1e522f9953ce78aeed7906a50
Vash The Stampede <aargancointepas@MacBook-Pro-de-Aargan.local>
2019-06-26 10:44:53 +0200

    Edit database

--- src/main/scala/database.scala
+++ src/main/scala/database.scala
@@ -42,7 +42,7 @@ object database {
 
   def createNewTable(): Unit = { // SQLite connection string
     // SQL statement for creating a new table
-    val sql_array = new Array[String](4)
+    val sql_array = new Array[String](5)
     sql_array(0) = "CREATE TABLE IF NOT EXISTS user (" +
       "	id_user integer PRIMARY KEY AUTOINCREMENT," +
       "	name_user text NOT NULL," +
@@ -59,11 +59,15 @@ object database {
       "	id_user integer NOT NULL," +
       "	giveaway text NOT NULL);"
 
-    sql_array(3) = "CREATE TABLE IF NOT EXISTS survey (" +
-      "	id_survey integer PRIMARY KEY AUTOINCREMENT," +
+    sql_array(3) = "CREATE TABLE IF NOT EXISTS survey_answer (" +
+      "	id_survey_answer integer PRIMARY KEY AUTOINCREMENT," +
       "	id_user integer NOT NULL," +
-      "	survey text NOT NULL," +
-      " response boolean NOT NULL);"
+      " response boolean NOT NULL," +
+      " id_survey int NOT NULL);"
+
+    sql_array(4) = "CREATE TABLE IF NOT EXISTS survey (" +
+      "	id_survey integer PRIMARY KEY AUTOINCREMENT," +
+      "	survey text NOT NULL);"
 
     for(sql <- sql_array) {
       try {
@@ -83,7 +87,7 @@ object database {
   }
 
   def insert(): Unit = {
-    val sql_array = new Array[String](20)
+    val sql_array = new Array[String](24)
     sql_array(0) = "INSERT INTO user(name_user,status_blacklist, status_sub) VALUES('Homer',0,1)"
     sql_array(1) = "INSERT INTO user(name_user,status_blacklist, status_sub) VALUES('Bart',0,1)"
     sql_array(2) = "INSERT INTO user(name_user,status_blacklist, status_sub) VALUES('Lisa',0,0)"
@@ -100,10 +104,14 @@ object database {
     sql_array(13) = "INSERT INTO giveaway(id_user,giveaway) VALUES(3,5)"
     sql_array(14) = "INSERT INTO giveaway(id_user,giveaway) VALUES(6,300)"
     sql_array(15) = "INSERT INTO giveaway(id_user,giveaway) VALUES(1,400)"
-    sql_array(16) = "INSERT INTO survey(id_user,survey,response) VALUES(1,'Aime tu le poulet ?',1)"
-    sql_array(17) = "INSERT INTO survey(id_user,survey,response) VALUES(3,'Aime tu le poulet ?',1)"
-    sql_array(18) = "INSERT INTO survey(id_user,survey,response) VALUES(4,'Aime tu le poulet ?',0)"
-    sql_array(19) = "INSERT INTO survey(id_user,survey,response) VALUES(5,'Aime tu le poulet ?',0)"
+    sql_array(16) = "INSERT INTO survey(survey) VALUES('Aime tu le poulet ?')"
+    sql_array(17) = "INSERT INTO survey(survey) VALUES('Aime tu le poisson ?')"
+    sql_array(18) = "INSERT INTO survey(survey) VALUES('Aime tu les burgers ?')"
+    sql_array(19) = "INSERT INTO survey_answer(id_user,response,id_survey) VALUES(1,1,1)"
+    sql_array(20) = "INSERT INTO survey_answer(id_user,response,id_survey) VALUES(2,1,1)"
+    sql_array(21) = "INSERT INTO survey_answer(id_user,response,id_survey) VALUES(3,0,2)"
+    sql_array(22) = "INSERT INTO survey_answer(id_user,response,id_survey) VALUES(1,1,1)"
+    sql_array(23) = "INSERT INTO survey_answer(id_user,response,id_survey) VALUES(5,0,3)"
 
     for(sql <- sql_array) {
       try {
