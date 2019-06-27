4e09e0e67a48639cb7feec7d73bc80cdb376bd3e
Vash The Stampede <aargancointepas@MacBook-Pro-de-Aargan.local>
2019-06-28 01:20:14 +0200

    Add test and edit survey

--- build.sbt
+++ build.sbt
@@ -14,5 +14,9 @@ libraryDependencies ++= Seq(
   "org.xerial" % "sqlite-jdbc" % "3.25.2" ,
   "com.typesafe.akka" %% "akka-http"   % "10.1.8",
   "com.typesafe.akka" %% "akka-stream" % "2.5.19",
-  "com.typesafe.akka" %% "akka-http-spray-json" % "10.1.8"
+  "com.typesafe.akka" %% "akka-http-spray-json" % "10.1.8",
+  "org.scalatest" %% "scalatest" % "3.0.5" % "test",
+  "com.typesafe.akka" %% "akka-stream-testkit" % "2.5.19",
+  "com.typesafe.akka" %% "akka-http-testkit" % "10.1.8"
 )
+
--- src/main/scala/survey.scala
+++ src/main/scala/survey.scala
@@ -3,12 +3,14 @@ import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
 import akka.Done
 import akka.http.scaladsl.server.Route
-import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Directives.{complete, _}
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import spray.json.DefaultJsonProtocol._
+import spray.json.JsValue
 import utils.sqliteUtils
 import utils.FromMap.to
+import utils.database
 
 import scala.io.StdIn
 
@@ -19,33 +21,162 @@ object survey {
   // needed for the future map/flatmap in the end and future in fetchItem and saveOrder
   implicit val executionContext = system.dispatcher
 
-  case class Donation(id_user: Int, sum_donation: Int)
-  case class Donations(vec: Vector[Donation])
-  implicit val donationFormat = jsonFormat2(Donation)
-  implicit val donationsFormat = jsonFormat1(Donations)
-
   case class User(id_user: Int, name_user: String, status_blacklist: Int, status_sub: Int)
   case class Users(vec: Vector[User])
   implicit val userFormat = jsonFormat4(User)
   implicit val usersFormat = jsonFormat1(Users)
 
-  def main(args: Array[String]) {
-    val url = "jdbc:sqlite:/Users/aargancointepas/Documents/ESGI-4IBD/ProgrammationFonctionnel/Project_Scala/db/project_scala.db"
-    val sqliteUtils = new sqliteUtils
+  case class Survey(id_survey: Int, survey: String, answer1: String, answer2: String)
+  case class Surveys(vec: Vector[Survey])
+  implicit val surveyFormat = jsonFormat4(Survey)
+  implicit val surveysFormat = jsonFormat1(Surveys)
 
-    val route: Route =
-      pathPrefix("getSumDonationByUser") {
-        get {
-          val req = sqliteUtils.query(url, "select id_user, name_user, status_blacklist, status_sub from user;", Seq("id_user","name_user", "status_blacklist", "status_sub"))
+  case class SurveyAnswer(id_survey_answer: Int, id_user: Int, response: Int, id_survey: Int)
+  case class SurveyAnswers(vec: Vector[Survey])
+  implicit val surveyAnswerFormat = jsonFormat4(SurveyAnswer)
+  implicit val surveyAnswersFormat = jsonFormat1(SurveyAnswers)
+
+  case class ResponseAnswer(survey: String, answer1: String, answer2: String, response: Int)
+  case class ResponseAnswers(vec: Vector[Survey])
+  implicit val responseAnswerFormat = jsonFormat4(ResponseAnswer)
+  implicit val responseAnswersFormat = jsonFormat1(ResponseAnswers)
+
+  case class ResponseSpeAnswer(survey: String, answer1: String, answer2: String, response: Int, id_survey: Int)
+  case class ResponseSpeAnswers(vec: Vector[Survey])
+  implicit val responseSpeAnswerFormat = jsonFormat5(ResponseSpeAnswer)
+  implicit val responseSpeAnswersFormat = jsonFormat1(ResponseSpeAnswers)
+
+  val url = "jdbc:sqlite:/Users/aargancointepas/Documents/ESGI-4IBD/ProgrammationFonctionnel/Project_Scala/db/project_scala.db"
+  val sqliteUtils = new sqliteUtils
+
+  val route_startDb: Route =
+    pathPrefix("startDB") {
+      get{
+        val startDb = new database
+        startDb.start_db()
+        complete("Database is ready")
+      }
+    }
+
+  val route_addSurvey: Route =
+    pathPrefix("addSurvey") {
+      post{
+        entity(as[JsValue]) { json =>
+          val survey = json.asJsObject.fields("survey").convertTo[String]
+          val answer1 = json.asJsObject.fields("answer1").convertTo[String]
+          val answer2 = json.asJsObject.fields("answer2").convertTo[String]
+          println("survey = " + survey)
+
+          val query_ct = "SELECT * FROM survey;"
+          println("query_ct = " + query_ct)
+
+          val res_ct = sqliteUtils.query(url, query_ct, Seq("id_survey", "survey", "answer1", "answer2"))
+          println("res_ct = " + res_ct)
+
+          val res_ctb = res_ct match {
+            case Some(r) => r.flatMap(v => to[Survey].from(v))
+              .count(x => x.survey == survey && x.answer1 == answer1 && x.answer2 == answer2)
+            case _ => None
+          }
+
+          if(res_ctb == 0){
+            val query = "INSERT INTO survey(survey, answer1, answer2) " +
+              "VALUES(\"" + survey.toString() + "\",\"" + answer1.toString + "\",\"" + answer2.toString + "\");"
+            println("query = " + query)
+            sqliteUtils.query(url, query, Seq())
+            complete(json)
+          }
+          else{
+            printf("Error survey already exist\n")
+            complete("Error survey already exist\n")
+          }
+        }
+      }
+    }
+
+  val route_addAnswer: Route =
+    pathPrefix("addAnswer") {
+      post{
+        entity(as[JsValue]) { json =>
+          val id_user = json.asJsObject.fields("id_user").convertTo[String]
+          val response = json.asJsObject.fields("response").convertTo[String]
+          val id_survey = json.asJsObject.fields("id_survey").convertTo[String]
+
+          val query_ct = "SELECT * FROM survey_answer "
+          //println("query_ct = " + query_ct)
+
+          val res_ct = sqliteUtils.query(url, query_ct, Seq("id_survey_answer", "id_user", "response", "id_survey"))
+
+          //println("res_ct = " + res_ct)
+
+          val res_ctb = res_ct match {
+            case Some(r) => r.flatMap(v => to[SurveyAnswer].from(v))
+              .count(x => x.id_user == id_user.toInt && x.response == response.toInt && x.id_survey == id_survey.toInt)
+            case _ => None
+          }
+
+          //println("res_ctb = " + res_ctb)
+
+          if(res_ctb.toString.toInt == 0){
+            val query = "INSERT INTO survey_answer(id_user, response, id_survey) " +
+              "VALUES(" + id_user.toString + "," + response.toString + "," + id_survey.toString + ");"
+            println("query = " + query)
+            sqliteUtils.query(url, query, Seq())
+            complete(json)
+          }
+          else{
+            printf("Error user has already participated\n")
+            complete("Error user has already participated\n")
+          }
+        }
+      }
+    }
+
+  val route_getResume: Route =
+    pathPrefix("getResume") {
+      get{
+        val req = sqliteUtils.query(url, "SELECT survey, answer1, answer2, response  FROM survey_answer a " +
+          "JOIN survey s WHERE a.id_survey = s.id_survey;", Seq("survey", "answer1", "answer2", "response"))
+        println("req = " + req)
+        req match {
+          case Some(r) => val values1 = r.flatMap(v => to[ResponseAnswer].from(v)).groupBy(x => (x.response, x.answer1, x.answer2, x.survey))
+            .map(t => (t._1._4.toString(), t._1._1 match { case 0 => t._1._2; case 1 => t._1._3 }, t._2.length.toString.toInt))
+            println("values1 = " + values1)
+            complete(values1)
+          //case Some(r) => val values1 = r.flatMap(v => to[SurveyAnswer].from(v))
+          //  complete(values1)
+          case None => complete("mauvaise table")
+        }
+      }
+    }
+
+  val route_getSpeResume: Route =
+    pathPrefix("getSpeResume") {
+      post{
+        entity(as[JsValue]) { json =>
+          val id_survey = json.asJsObject.fields("id_survey").convertTo[String]
+          val req = sqliteUtils.query(url, "SELECT survey, answer1, answer2, response, s.id_survey  FROM survey_answer a " +
+            "JOIN survey s WHERE a.id_survey = s.id_survey;", Seq("survey", "answer1", "answer2", "response", "id_survey"))
+          println("req = " + req)
           req match {
-            case Some(r) => val values = r.flatMap(v => to[User].from(v))
-              complete(Users(values))
+            case Some(r) => val values1 = r.flatMap(v => to[ResponseSpeAnswer].from(v)).filter(x => x.id_survey == id_survey.toInt).groupBy(x => (x.response, x.answer1, x.answer2, x.survey))
+              .map(t => (t._1._4.toString(), t._1._1 match {
+                case 0 => t._1._2;
+                case 1 => t._1._3
+              }, t._2.length.toString.toInt))
+              println("values1 = " + values1)
+              complete(values1)
+            //case Some(r) => val values1 = r.flatMap(v => to[SurveyAnswer].from(v))
+            //  complete(values1)
             case None => complete("mauvaise table")
           }
         }
       }
+    }
 
-    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+  def main(args: Array[String]) {
+    val combineRoute = route_addSurvey ~ route_addAnswer ~ route_getResume ~ route_getSpeResume ~ route_startDb
+    val bindingFuture = Http().bindAndHandle(combineRoute, "localhost", 8080)
     println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
     StdIn.readLine() // let it run until user presses return
     bindingFuture
